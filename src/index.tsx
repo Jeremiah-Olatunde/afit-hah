69754f912cdaec5b5bfe0f0a06549c5e89369768
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-01 18:10:15 +0100

    refactor: refactor alpine counter

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { serveStatic } from "hono/bun";
 import { jsxRenderer } from "hono/jsx-renderer";
 
 import { Base } from "./views/Base";
+import { Counter } from "./views/Counter";
 
 const app = new Hono();
 
@@ -13,28 +14,7 @@ app.get("/public/*", serveStatic({ root: "./src" }));
 function Home() {
 	return (
 		<>
-			<AlpineCounter />
-		</>
-	);
-}
-
-function AlpineCounter() {
-	return (
-		<>
-			<div class="w-screen h-screen grid items-center justify-center">
-				<div x-data="{ count: 0 }" class="flex gap-2">
-					<button
-						x-on:click="count++"
-						class="bg-red-400 rounded-md px-4 py-2 text-3xl text-white font-bold"
-					>
-						increment
-					</button>
-					<div
-						x-text="count"
-						class="bg-blue-400 text-3xl px-4 py-2 text-white font-bold rounded-md"
-					></div>
-				</div>
-			</div>
+			<Counter />
 		</>
 	);
 }
