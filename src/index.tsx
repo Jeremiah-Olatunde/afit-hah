c2b624e4e6a15b709978e77445c2054db8085dc1
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-01 18:05:23 +0100

    feat: add styling for alpine counter

--- src/index.tsx
+++ src/index.tsx
@@ -21,10 +21,19 @@ function Home() {
 function AlpineCounter() {
 	return (
 		<>
-			<div x-data="{ count: 0 }">
-				<button x-on:click="count++">Increment</button>
-
-				<span x-text="count"></span>
+			<div class="w-screen h-screen grid items-center justify-center">
+				<div x-data="{ count: 0 }" class="flex gap-2">
+					<button
+						x-on:click="count++"
+						class="bg-red-400 rounded-md px-4 py-2 text-3xl text-white font-bold"
+					>
+						increment
+					</button>
+					<div
+						x-text="count"
+						class="bg-blue-400 text-3xl px-4 py-2 text-white font-bold rounded-md"
+					></div>
+				</div>
 			</div>
 		</>
 	);
