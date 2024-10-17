b8178afe9396070532534cbde7a3eacdb36a9696
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-17 22:36:34 +0100

    feat(login): add toMessage function

--- src/forms/login.tsx
+++ src/forms/login.tsx
@@ -8,3 +8,31 @@ const Message = z
 	.strict();
 
 type Message = z.infer<typeof Message>;
+
+type MessageErr = {
+	status: "error";
+	messages: string[];
+};
+
+type MessageOk = {
+	status: "ok";
+	data: Message;
+};
+
+type MessageResult = MessageErr | MessageOk;
+
+function toMessage(form: unknown): MessageResult {
+	const parsed = Message.safeParse(form);
+
+	if (!parsed.success) {
+		return {
+			status: "error",
+			messages: parsed.error.issues.map((issue) => issue.message),
+		};
+	}
+
+	return {
+		status: "ok",
+		data: parsed.data,
+	};
+}
